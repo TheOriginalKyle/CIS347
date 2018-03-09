660f7ae8b6df127273692f262a4a0b80d00cf14d
TheOriginalKyle <2002kbohlander@gmail.com>
2018-03-08 19:03:48 -0800

    Added more informative comments.

--- CIS347v2.py
+++ CIS347v2.py
@@ -7,10 +7,15 @@ import datetime
 from jnpr.junos import Device
 from jnpr.junos.utils.config import Config
 from jnpr.junos.factory.factory_loader import FactoryLoader
+
+# This creates a log file titled 'CIS347.log', sets the level to debug and formats it for 'current time | how big the issue is | the actual message'.
+# See https://docs.python.org/2/library/logging.html
 logging.basicConfig(filename='CIS347.log',level=logging.DEBUG,format='%(asctime)s | %(levelname)s | %(message)s')
+# This script will crash if this file does not exist.
 file_object = open('CIS347-Device-List.log', 'a')
 
-#OpenDayLight RESTCONF API settings.
+# OpenDayLight RESTCONF API settings.
+# SET THESE TO MATCH YOUR CONFIG.
 odl_ip = "192.168.140.253"
 username = "admin"
 password = "admin"
@@ -24,11 +29,17 @@ response = requests.get(odl_url, auth=(odl_username, odl_password))
 # Find information about nodes in retrieved JSON file.
 odl_macs = []
 
+# If the script crashes in one of these two for loops you are parsing the json incorrectly. (Probably because you're running a different version of ODL.)
+# Uncomment one of these if its parsing incorrectly.
+# print response.json()
+# print response.json()['network-topology']['topology']
 for nodes in response.json()['network-topology']['topology'][1]:
     # Walk through all node information.
     node_info = response.json()['network-topology']['topology'][1]
     # Look for Mac and IP addresses in node information.
     for node in node_info['node']:
+        # If the script crashes in this for loop you would have no idea.
+        # This is because of except: pass you'll know if it crashes if nothing is printed from odl parse.
         try:
             ip_address = node['host-tracker-service:addresses'][0]['ip']
             mac_address = node['host-tracker-service:addresses'][0]['mac']
@@ -59,6 +70,7 @@ EtherSwView:
     interface: mac-interfaces-list/mac-interfaces
 '''
 # Login to switch
+# CHANGE THIS TO MATCH YOUR CONFIG.
 host = "192.168.140.1"
 switch_user = "root"
 switch_password = "Admin12345"
@@ -82,6 +94,8 @@ for i in table:
     print ('\n')
 
 # Compare MACs from ODL and EthernetSwitchingTable Table
+# If you never see anything from the cu.diff() line thats because odl_macs is empty.
+# Probably because your on a different version and the JSON is different than the one this script is written for. (Nitrogen)
 mac_set = [i for e in odl_macs for i in mac_table if e in i]
 
 # Automate the port security for each entry in final list.
@@ -94,15 +108,21 @@ for i in mac_set:
     interface = [i[:-2] for i in interface]
     new_interface = interface.pop()
     today = datetime.datetime.now()
-    #Writes down all mac binded devices to a log file
+    # Writes down all mac binded devices to a log file.
+    # This script will crash if the file does not exist.
+    # Format is 'Current time in year-month-day - Hour:Minute:Second,millisecond | Binding Mac | *some mac address* | *some interface*
     file_object.write(today.strftime('%Y-%m-%d - %H:%M:%S,%f') + ' | ' + 'Binding Mac | ' + new_mac + ' | ' + new_interface + '\n')
+    # Adds the mac that will be locked on that interface to the config.
     config_add.append('set interface %s allowed-mac %s' % (new_interface, new_mac))
-    #Change to delete to turn led off
+    # Change to delete to turn led off.
     config_light.append('set poe interface %s' % (new_interface))
 
+# Puts the config in the format Junos is expecting.
 set_add = '\n'.join(map(str,config_add))
+# These are seperate due to the way config_script sets it.
 light_add = '\n'.join(map(str,config_light))
 
+# Probably could merge this with set_add but ¯\_(ツ)_/¯
 config_script = """
 edit ethernet-switching-options secure-access-port
 %s
@@ -110,12 +130,17 @@ edit ethernet-switching-options secure-access-port
 
 # Load and Commit the configuration to the switch
 cu = Config(dev)
+# Merges the config with the currently running config. 
+# Probably could've merged light_add with config_script but if it aint broke.
 cu.load(config_script, format="set", merge=True)
 cu.load(light_add, format="set", merge=True)
+# This try catch is here in case there's no difference between the running config and the proposed one.
+# If you never see a difference and the ports aren't locking you are on a different version of ODL.
 try:
     print 'These are the changes:\n ' + cu.diff()
 except:
     print 'Nothing changed?'
+# Commits the config. This could be put in the try catch but it won't hurt Junos to commit one with no changes.
 cu.commit()
 print "Configuration Successful! Goodbye."
 dev.close()
